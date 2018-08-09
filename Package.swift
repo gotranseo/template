c4f90a05973ee912088bcbceb3506802e497f4d4
Jimmy McDermott <mcdappdev@gmail.com>
2018-08-09 14:36:24 -0500

    Update deps

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc.3"),
         .package(url: "https://github.com/gotranseo/flash.git", from: "3.0.1"),
         .package(url: "https://github.com/vapor/url-encoded-form.git", from: "1.0.0"),
-        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc.2"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
         .package(url: "https://github.com/brokenhandsio/VaporSecurityHeaders.git", from: "2.0.0")
     ],
     targets: [
